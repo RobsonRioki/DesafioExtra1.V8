c31716f844d9c38d44aa7ffa5469b1c88715ef4a
RobsonRioki <robson.nakama@sptech.school>
2024-02-22 14:38:55 -0300

    Retirei a descricao da atividade e arrumei o bug da sobreposicao do objeto

--- app.js
+++ app.js
@@ -1,49 +1,30 @@
-function verificar (){
-
-}
-
-let atividadesNew = JSON.parse(localStorage.getItem('atividades'));
-if (!atividadesNew){
-    atividadesNew =[{}];
-}   
-
 function registrar(){
     var nomeAtividade = inputAtividade.value.trim();
     var prazoAtividade = inputPrazo.value;
-    var descricaoAtividade = inputDescricao.value.trim();
+    let atividadesNew = JSON.parse(localStorage.getItem('atividades'));
+    if (!atividadesNew){
+    atividadesNew =[];
+    } 
 
-    
-    if (nomeAtividade != "" && prazoAtividade != "" && descricaoAtividade != ""){
-        const novaAtividade = {nome: nomeAtividade.toString(), prazo: prazoAtividade.toString(), descricao: descricaoAtividade.toString()};
-
+    if (nomeAtividade != "" && prazoAtividade != ""){
+        const novaAtividade = {nome: nomeAtividade.toString(), prazo: prazoAtividade.toString()};
         atividadesNew.push(novaAtividade);
         localStorage.setItem('atividades' , JSON.stringify(atividadesNew));
 
-        console.log(novaAtividade);
-
-        // atividadesNew 
-        //adicionar as novas atividades aqui neste objeto
-        console.log(atividadesNew);
-    
-
-        // localStorage.setItem('atividades', JSON.stringify(atividadesNew));
-
-
-        // location.reload() 
-        // alert("Atividade registrada!");
+        location.reload() 
+        alert("Atividade registrada!");
     }else{
         alert("Preencha todos os campos!")
     }
-
 }
 
-
 function carregarMural(){
-    if (JSON.parse(localStorage.getItem('atividades')) == 'null'){
+    if (JSON.parse(localStorage.getItem('atividades')) == null){
         console.log('nada adicionado');
     }else{
-// alert('mural carregado');
-muralAtividades.innerHTML = JSON.stringify(localStorage.getItem('atividades'));;
-console.log(JSON.stringify(localStorage.getItem('atividades')));
+    // alert('mural carregado');
+
+    muralAtividades.innerHTML = JSON.stringify(localStorage.getItem('atividades'));
+    console.log(JSON.stringify(localStorage.getItem('atividades')));
     }
 }
