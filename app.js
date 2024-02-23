87a32901903728226973930c10707f9d0cfceae9
RobsonRioki <robson.nakama@sptech.school>
2024-02-23 13:01:18 -0300

    [200~

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ function registrar(){
     } 
 
     if (nomeAtividade != "" && prazoAtividade != ""){
-        const novaAtividade = {nome: nomeAtividade.toString(), prazo: prazoAtividade.toString()};
+        const novaAtividade = {nome: nomeAtividade.toString(), prazo: prazoAtividade.toString(), concluida: false};
         atividadesNew.push(novaAtividade);
         localStorage.setItem('atividades' , JSON.stringify(atividadesNew));
 
@@ -18,12 +18,36 @@ function registrar(){
     }
 }
 
-function carregarMural(){
-    if (JSON.parse(localStorage.getItem('atividades')) == null){
-        console.log('nada adicionado');
-    }else{
-    // alert('mural carregado');
-    muralAtividades.innerHTML = JSON.stringify(localStorage.getItem('atividades'));
-    console.log(JSON.stringify(localStorage.getItem('atividades')));
+function carregarMural() {
+    const atividades = JSON.parse(localStorage.getItem('atividades')) || [];
+
+    if (atividades.length === 0) {
+        console.log('Nada adicionado');
+    } else {
+        for (let i = 0; i < atividades.length; i++) {
+            const atividade = atividades[i];
+            const statusConcluida = atividade.concluida ? "concluida" : "";
+
+            muralAtividades.innerHTML += `<div class="atividadeNoMural ${statusConcluida}">
+                ${atividade.nome} ${atividade.prazo}
+                <button id="botaoRemover${i}" onclick="remover(${i})" class="botaoRemover">Remover</button>
+                <button onclick="concluir(${i})" class="botaoAlterar" id="botaoAlterar${i}">Concluir</button>
+                <br>
+            </div>`;
+        }
     }
+}
+function remover(index) {
+    let atividades = JSON.parse(localStorage.getItem('atividades')) || [];
+        atividades.splice(index, 1);
+        localStorage.setItem('atividades', JSON.stringify(atividades));
+        location.reload();
+
+}
+
+function concluir(index) {
+    let atividades = JSON.parse(localStorage.getItem('atividades')) || [];
+    atividades[index].concluida = true;
+    localStorage.setItem('atividades', JSON.stringify(atividades));
+    location.reload();
 }
